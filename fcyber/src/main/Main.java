0afd727cf9c4c4b5177a584c9fad196bcaef56d7
Ömer Faruk Koç <77229286+Fcyb3r@users.noreply.github.com>
2021-01-25 04:09:01 +0300

    methodlarlaCalisma2

---
+++ fcyber/src/main/Main.java
@@ -0,0 +1,49 @@
+package main;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		basla();
+		gec();
+		bekle();
+		guncelle();
+		yeniEkran();
+		geri();
+	}
+
+	public static void gec() {
+
+		System.out.println("Gecildi");
+
+	}
+
+	public static void bekle() {
+		System.out.println("Bekleniyor");
+
+	}
+
+	public static void guncelle() {
+		System.out.println("Guncelleniyor");
+	}
+
+	public static void yeniEkran() {
+
+		System.out.println("Yeni ekran");
+	}
+
+	public static void geri() {
+
+		System.out.println("Geri gidildi");
+
+	}
+
+	public static void basla() {
+
+		System.out.println("------------------------");
+		System.out.println("*********Basladı*********");
+		System.out.println("------------------------");
+	}
+}
+
+//methodları emir kipi ile kullandım
